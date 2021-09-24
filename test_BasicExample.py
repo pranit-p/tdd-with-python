56362d063ed6dbe18de6cad4dae26f12663ada4e
pranit pawar <82363887+pranit-p@users.noreply.github.com>
2021-09-24 09:37:47 +0530

    Update test_BasicExample.py

--- test_BasicExample.py
+++ test_BasicExample.py
@@ -5,7 +5,7 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(False,False)
     def test_something11(self):
-        self.assertEqual(True,False)
+        self.assertEqual(False,False)
 
 
 if __name__ == '__main__':
