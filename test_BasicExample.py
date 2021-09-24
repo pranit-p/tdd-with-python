3725234870cdeed00817baf3485c60fc7c571910
pranit pawar <82363887+pranit-p@users.noreply.github.com>
2021-09-24 09:34:48 +0530

    Update test_BasicExample.py

--- test_BasicExample.py
+++ test_BasicExample.py
@@ -4,7 +4,7 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(False,False)
-    def test_something(self):
+    def test_something11(self):
         self.assertEqual(True,False)
 
 
